724575705465efda82adf15e230f189b39a65af9
Adam Hallett <adam.t.hallett@gmail.com>
2019-12-17 08:48:54 -0600

    bundle gem tiny_tds_wrapper

---
+++ lib/tiny_tds_wrapper.rb
@@ -0,0 +1,6 @@
+require "tiny_tds_wrapper/version"
+
+module TinyTdsWrapper
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/tiny_tds_wrapper/version.rb
@@ -0,0 +1,3 @@
+module TinyTdsWrapper
+  VERSION = "0.1.0"
+end
